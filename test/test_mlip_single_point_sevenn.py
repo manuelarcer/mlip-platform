93b84929a7533c31a37efe9342b89353ba57edd7
YuanZhang28 <leey0246@e.ntu.edu.sg>
2025-06-09 13:43:55 +0800

    update

--- test/test_mlip_single_point_sevenn.py
+++ test/test_mlip_single_point_sevenn.py
@@ -1,15 +1,12 @@
+import pytest
 from ase.io import read
-from sevenn.calculator import SevenNetCalculator
 
-def test_mlip_single_point():
-    # Load a small POSCAR file for testing
-    atoms = read("/Users/leeyuanzhang/Documents/mlip-platform-1/test/POSCAR", format="vasp")
+def test_mlip_single_point_sevenn():
+    atoms = read("test/POSCAR", format="vasp")
 
-    # Attach SevenNet calculator
+    # Only import here, so pytest doesn't crash during collection
+    from sevenn.calculator import SevenNetCalculator
+
     atoms.calc = SevenNetCalculator("7net-mf-ompa", modal="mpa")
-
-    # Run energy calculation
     energy = atoms.get_potential_energy()
-
-    # Check that result is a float
     assert isinstance(energy, float)
