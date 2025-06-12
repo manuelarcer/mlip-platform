67360265eef6af0b6b8ff87a141986c1492e2a88
YuanZhang28 <leey0246@e.ntu.edu.sg>
2025-06-12 15:51:22 +0800

    changed minor error on the code

--- src/mlip_platform/neb.py
+++ src/mlip_platform/neb.py
@@ -1,16 +1,14 @@
 from ase.io import read, write
 from ase.mep import NEB
-from ase.mep.neb import idpp_interpolate
+from ase.mep.neb import idpp_interpolate  
 from ase.optimize import BFGS, MDMin, FIRE
 from sevenn.calculator import SevenNetCalculator
-#from chgnet.model import CHGNetCalculator
-#from chgnet.model import CHGNet
 import pandas as pd
 
 class CustomNEB():
     def __init__(self, initial, final, num_images=9, interp_fmax=0.1, interp_steps=1000, fmax=0.05, mlip='sevenn-mf-ompa'):
         self.initial = initial
-        # initial and final may have different cell sizes. Resize final state to initial, scale atoms accordingly
+        # Resize final to match initial
         final.set_cell(initial.get_cell(), scale_atoms=True)
         self.final = final
         self.num_images = num_images
@@ -18,41 +16,35 @@ class CustomNEB():
         self.interp_steps = interp_steps
         self.fmax = fmax
         self.mlip = mlip
-        self.images = self.setup_neb()   
+        self.images = self.setup_neb()
 
     def setup_calculator(self, model='sevenn-mf-ompa'):
         if model == 'sevenn-mf-ompa':
             calc = SevenNetCalculator('7net-mf-ompa', modal='mpa')
-        elif model == 'chgnet':
-            calc = CHGNetCalculator()
+            return calc
         else:
-            print(f"Unknown model: {model}. Using default.")
-            calc = SevenNetCalculator('7net-mf-ompa', modal='mpa')
-        return calc
+            raise ValueError(f"Unknown MLIP model: {model}")
 
     def setup_neb(self):
-        images = [self.initial] + [self.initial.copy() for _ in range(self.num_images - 2)] + [self.final]
-        idpp_interpolate(images, traj='idpp.traj', log='idpp.log', fmax=self.interp_fmax, mic=True, steps=self.interp_steps)
-        return images
-    
-    def run_neb(self, optimizer=MDMin, trajectory='A2B.traj', climb=False):
-        # Set up NEB
-        neb = NEB(self.images, climb=climb)
-        for image in self.images:
-            image.calc = self.setup_calculator(self.mlip)
-        optimizer = optimizer(neb, trajectory=trajectory)
-        optimizer.run(fmax=self.fmax)
-        return self.images
-    
-    def process_results(self):
-        results = {'i': [], 'e': []}
-        for i, image in enumerate(self.images):
-            results['i'].append(i)
-            results['e'].append(image.get_potential_energy())
-        df = pd.DataFrame(results)
-        df['rel_e'] = df['e'] - df['e'].min()
-        return df
+        # Generate images
+        images = [self.initial]
+        for _ in range(self.num_images - 2):
+            images.append(self.initial.copy())
+        images.append(self.final)
 
-    def write_images(self, path='images'):
-        for i, image in enumerate(self.images):
-            write(f'{path}/{i:02d}.vasp', image, format='vasp')
+        # Assign calculator to each image
+        calc = self.setup_calculator(self.mlip)
+        for image in images:
+            image.calc = calc
+
+        self.neb = NEB(images)
+        return images
+
+    def interpolate_idpp(self):  
+        """Interpolate NEB images using IDPP (Improved Dimer Projection Path)."""
+        idpp_interpolate(self.images)
+
+    def run(self):
+        """Run NEB optimization using the FIRE optimizer."""
+        optimizer = FIRE(self.neb)
+        optimizer.run(fmax=self.fmax)
