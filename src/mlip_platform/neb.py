7969a3df27093768192a4260734267559b2cbf4e
YuanZhang28 <leey0246@e.ntu.edu.sg>
2025-06-18 10:04:26 +0800

    Updated neb.py file

--- src/mlip_platform/neb.py
+++ src/mlip_platform/neb.py
@@ -1,12 +1,13 @@
 from ase.io import read, write
 from ase.mep import NEB
 from ase.mep.neb import idpp_interpolate
-from ase.optimize import FIRE
+from ase.optimize import BFGS, MDMin, FIRE
 import pandas as pd
 
 class CustomNEB():
     def __init__(self, initial, final, num_images=9, interp_fmax=0.1, interp_steps=1000, fmax=0.05, mlip='sevenn-mf-ompa'):
         self.initial = initial
+        # Resize final to match initial cell
         final.set_cell(initial.get_cell(), scale_atoms=True)
         self.final = final
         self.num_images = num_images
@@ -27,23 +28,27 @@ class CustomNEB():
             raise ValueError(f"Unsupported MLIP model: {model}")
 
     def setup_neb(self):
-        images = [self.initial]
-        for _ in range(self.num_images - 2):
-            images.append(self.initial.copy())
-        images.append(self.final)
-
-        calc = self.setup_calculator(self.mlip)
-        for image in images:
-            image.calc = calc
-
-        self.neb = NEB(images)
+        images = [self.initial] + [self.initial.copy() for _ in range(self.num_images - 2)] + [self.final]
+        idpp_interpolate(images, traj='idpp.traj', log='idpp.log', fmax=self.interp_fmax, mic=True, steps=self.interp_steps)
         return images
 
-    def interpolate_idpp(self):
-        """Interpolate NEB images using IDPP (Improved Dimer Projection Path)."""
-        idpp_interpolate(self.images)
-
-    def run(self):
-        """Run NEB optimization using the FIRE optimizer."""
-        optimizer = FIRE(self.neb)
+    def run_neb(self, optimizer=MDMin, trajectory='A2B.traj', climb=False):
+        neb = NEB(self.images, climb=climb)
+        for image in self.images:
+            image.calc = self.setup_calculator(self.mlip)
+        optimizer = optimizer(neb, trajectory=trajectory)
         optimizer.run(fmax=self.fmax)
+        return self.images
+
+    def process_results(self):
+        results = {'i': [], 'e': []}
+        for i, image in enumerate(self.images):
+            results['i'].append(i)
+            results['e'].append(image.get_potential_energy())
+        df = pd.DataFrame(results)
+        df['rel_e'] = df['e'] - df['e'].min()
+        return df
+
+    def write_images(self, path='images'):
+        for i, image in enumerate(self.images):
+            write(f'{path}/{i:02d}.vasp', image, format='vasp')
