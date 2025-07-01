4ad858135be29cb3a97f5b5d150bf27ed10865d7
YuanZhang28 <leey0246@e.ntu.edu.sg>
2025-07-01 11:35:59 +0800

    updateed core and command neb

--- src/mlip_platform/core/md.py
+++ src/mlip_platform/core/md.py
@@ -1,3 +1,4 @@
+import sys
 from ase.md.verlet import VelocityVerlet
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
 from ase.md import MDLogger
@@ -9,14 +10,20 @@ def initialize_md(atoms, temperature_K=300, timestep_fs=2.0):
     dyn = VelocityVerlet(atoms, timestep=timestep_fs * units.fs)
     return dyn
 
-def attach_logger(dyn, atoms, log_path, interval=5, stress=False):
-    """Attach a logger to the MD run."""
-    logger = MDLogger(dyn, atoms, log_path, header=True, stress=stress)
-    dyn.attach(logger, interval=interval)
+def attach_logger(dyn, atoms, log_path=None, interval=5, stress=False):
+    """Attach one or more loggers to the MD run."""
+    # Always log to terminal
+    stdout_logger = MDLogger(dyn, atoms, sys.stdout, header=True, stress=stress)
+    dyn.attach(stdout_logger, interval=interval)
 
-def run_md(atoms, log_path, temperature_K=300, timestep_fs=2.0, steps=10, interval=5, stress=False):
+    # Optionally also log to file
+    if log_path:
+        file_logger = MDLogger(dyn, atoms, log_path, header=True, stress=stress)
+        dyn.attach(file_logger, interval=interval)
+
+def run_md(atoms, log_path=None, temperature_K=300, timestep_fs=2.0, steps=10, interval=5, stress=False):
     """Run an MD simulation with logging."""
     dyn = initialize_md(atoms, temperature_K=temperature_K, timestep_fs=timestep_fs)
-    attach_logger(dyn, atoms, log_path, interval=interval, stress=stress)
+    attach_logger(dyn, atoms, log_path=log_path, interval=interval, stress=stress)
     dyn.run(steps)
     return dyn
