e31dbd32633b794e01f10371e1372bec9580faa4
YuanZhang28 <leey0246@e.ntu.edu.sg>
2025-07-07 17:43:29 +0800

    update cli main.py

--- src/mlip_platform/cli/main.py
+++ src/mlip_platform/cli/main.py
@@ -5,19 +5,11 @@ app = typer.Typer()
 
 # Register subcommands
 app.add_typer(benchmark.app, name="benchmark", help="Run MLIP benchmark on a structure" \
-"PYTHONPATH=src python -m mlip_platform.cli.main benchmark run tests/fixtures/structures/POSCAR")
-app.add_typer(neb.app, name="neb", help="Run NEB interpolation and relaxation " \
-"PYTHONPATH=src python -m mlip_platform.cli.main neb run \
-  --initial tests/fixtures/structures/fragment_initial.vasp \
-  --final tests/fixtures/structures/fragment_final.vasp \
-  --mlip 7net-mf-ompa or mace ")
+)
+app.add_typer(neb.app, name="neb", help="Run NEB interpolation and relaxation " 
+)
 app.add_typer(md.app, name="md", help="Run MD simulations "
-"PYTHONPATH=src python -m mlip_platform.cli.main md run \
-  --structure tests/fixtures/structures/fragment_initial.vasp \
-  --model mace \
-  --steps 10 \
-  --temperature 300 \
-  --log mace_md.log or --log seven_md.log")  
+)  
 
 if __name__ == "__main__":
     app()
